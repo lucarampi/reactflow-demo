2c7f43c622b006bb923282de006e60266692bda8
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2023-08-23 23:21:08 -0300

    bETTER STYLE AND DARK MODE

--- app/page.tsx
+++ app/page.tsx
@@ -66,12 +66,16 @@ const initialEdges = [
   { id: "e2-3", source: "2", target: "3", animated: true },
 ];
 
+const initBgColor = "#1A192B";
+
 export default function App() {
   const [isMinimapEnabled, setIsMinimapEnabled] = useState(false);
   const [isControlEnabled, setIsControlEnabled] = useState(false);
   const [selectedBackgroundVariant, setSelectedBackgroundVariant] = useState(
     BackgroundVariant.Dots
   );
+  const [bgColor, setBgColor] = useState(initBgColor);
+  const [isAutoBgColor, setIsAutoBgColor] = useState<"AUTO" | "MANUAL">("AUTO");
   const [nodes, setNodes] = useState(initialNodes);
   const [edges, setEdges] = useState(initialEdges);
 
@@ -93,17 +97,19 @@ export default function App() {
   );
 
   const defaultEdgeOptions: DefaultEdgeOptions = { animated: false };
-
+  const customStyle = {
+    backgroundColor: bgColor,
+  };
   return (
     <div className="flex flex-col w-screen h-screen">
-      <div className="w-full h-fit bg-white p-2">
+      {/* <div className="w-full h-fit bg-white p-2">
         <Link
           className="bg-blue-200 py-1 px-2 rounded text-blue-700 border-blue-700 border"
           href={""}
         >
           Default
         </Link>
-      </div>
+      </div> */}
       <div className="w-full h-full">
         <ReactFlow
           nodes={nodes}
@@ -112,7 +118,12 @@ export default function App() {
           onEdgesChange={onEdgesChange}
           onConnect={onConnect}
           defaultEdgeOptions={defaultEdgeOptions}
-          className="bg-slate-100"
+          className={`${
+            isAutoBgColor === "MANUAL"
+              ? ``
+              : " bg-slate-50 dark:bg-slate-950 "
+          } transition-colors `}
+          style={{ ...(isAutoBgColor === "MANUAL" ? customStyle : {}) }}
         >
           {isControlEnabled && <Controls />}
 
@@ -129,42 +140,83 @@ export default function App() {
             className="bg-transparent flex flex-col gap-6 select-none"
             position="top-right"
           >
-            <div className="bg-white pt-2 pb-4 px-4 rounded-lg border-2 shadow-xl border-blue-500 flex flex-col gap-2 ">
-              <div className="font-semibold mb-1">Plugins</div>
-              <ToggleButton
-                checkedValue={isMinimapEnabled}
-                setCheckedValue={setIsMinimapEnabled}
-                label="Minimap"
-              />
-              <ToggleButton
-                checkedValue={isControlEnabled}
-                setCheckedValue={setIsControlEnabled}
-                label="Controls"
-              />
+            <div className="bg-white  dark:bg-slate-900 pb-4  rounded-lg border-2 shadow-xl border-blue-500 dark:border-slate-500   flex flex-col gap-2 ">
+              <div className="font-semibold mb-1 rounded-t-lg text-lg w-full border-b-2 py-1 text-blue-800 dark:text-slate-50 bg-blue-100  dark:bg-slate-800  border-blue-500 dark:border-slate-500 px-2 text-center ">
+                Plugins
+              </div>
+              <div className=" px-4 flex flex-col gap-2  ">
+                <ToggleButton
+                  checkedValue={isMinimapEnabled}
+                  setCheckedValue={setIsMinimapEnabled}
+                  label="Minimap"
+                />
+                <ToggleButton
+                  checkedValue={isControlEnabled}
+                  setCheckedValue={setIsControlEnabled}
+                  label="Controls"
+                />
+              </div>
             </div>
-            <div className="bg-white pt-2 pb-4 px-4 rounded-lg border-2 shadow-xl border-blue-500 flex flex-col gap-2 ">
-              <div className="font-semibold mb-1">Background</div>
-              <RadioButton
-                label="Dots"
-                checkedValue={selectedBackgroundVariant}
-                groupName="BackgroundVariant"
-                setCheckedValue={setSelectedBackgroundVariant}
-                value={BackgroundVariant.Dots}
-              />
-              <RadioButton
-                label="Cross"
-                checkedValue={selectedBackgroundVariant}
-                groupName="BackgroundVariant"
-                setCheckedValue={setSelectedBackgroundVariant}
-                value={BackgroundVariant.Cross}
-              />
-              <RadioButton
-                label="Lines"
-                checkedValue={selectedBackgroundVariant}
-                groupName="BackgroundVariant"
-                setCheckedValue={setSelectedBackgroundVariant}
-                value={BackgroundVariant.Lines}
-              />
+
+            <div className="bg-white  dark:bg-slate-900 pb-4  rounded-lg border-2 shadow-xl border-blue-500 dark:border-slate-500   flex flex-col gap-2 ">
+              <div className="font-semibold mb-1 rounded-t-lg text-lg w-full border-b-2 py-1 text-blue-800 dark:text-slate-50 bg-blue-100  dark:bg-slate-800  border-blue-500 dark:border-slate-500 px-2 text-center ">
+                Texture
+              </div>
+              <div className=" px-4 flex flex-col gap-2  ">
+                <RadioButton
+                  label="Dots"
+                  checkedValue={selectedBackgroundVariant}
+                  groupName="BackgroundVariant"
+                  setCheckedValue={setSelectedBackgroundVariant}
+                  value={BackgroundVariant.Dots}
+                />
+                <RadioButton
+                  label="Cross"
+                  checkedValue={selectedBackgroundVariant}
+                  groupName="BackgroundVariant"
+                  setCheckedValue={setSelectedBackgroundVariant}
+                  value={BackgroundVariant.Cross}
+                />
+                <RadioButton
+                  label="Lines"
+                  checkedValue={selectedBackgroundVariant}
+                  groupName="BackgroundVariant"
+                  setCheckedValue={setSelectedBackgroundVariant}
+                  value={BackgroundVariant.Lines}
+                />
+              </div>
+            </div>
+            <div className="bg-white  dark:bg-slate-900 pb-4  rounded-lg border-2 shadow-xl border-blue-500 dark:border-slate-500   flex flex-col gap-2 ">
+              <div className="font-semibold mb-1 rounded-t-lg text-lg w-full border-b-2 py-1 text-blue-800 dark:text-slate-50 bg-blue-100  dark:bg-slate-800  border-blue-500 dark:border-slate-500 px-2 text-center ">
+                Background
+              </div>
+              <div className=" px-4 flex flex-col gap-2  ">
+                <RadioButton
+                  label="Auto"
+                  checkedValue={isAutoBgColor}
+                  groupName="BackgroundColorSelectMode"
+                  setCheckedValue={setIsAutoBgColor}
+                  value={"AUTO"}
+                />
+                <RadioButton
+                  label="Manual"
+                  checkedValue={isAutoBgColor}
+                  groupName="BackgroundColorSelectMode"
+                  setCheckedValue={setIsAutoBgColor}
+                  value={"MANUAL"}
+                />
+                <div>
+                  <input
+                    className={`w-full p-0 m-0 outline-none stroke-none rounded-full  border-transparent outline-transparent  bg-transparent cursor-pointer `}
+                    id="nativeColorPicker1"
+                    type="color"
+                    value={bgColor}
+                    onChange={(ev) => {
+                      setBgColor(ev.target.value);
+                    }}
+                  />
+                </div>
+              </div>
             </div>
           </Panel>
 
--- components/RadioButton.tsx
+++ components/RadioButton.tsx
@@ -1,9 +1,9 @@
 import { BackgroundVariant } from "reactflow";
 
-interface RadioButtonProps {
+interface RadioButtonProps{
   label: string;
-  value: BackgroundVariant;
-  checkedValue: BackgroundVariant;
+  value: string;
+  checkedValue: string;
   groupName: string;
   setCheckedValue: any;
 }
@@ -25,11 +25,11 @@ export default function RadioButton({
           value={value}
           onChange={(ev) => setCheckedValue(ev.currentTarget.value)}
           name={groupName}
-          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 "
+          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300   dark:bg-gray-700 dark:border-gray-600"
         />
         <label
           htmlFor={value}
-          className="ml-2 text-sm font-medium text-gray-900 "
+          className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
         >
           {label}
         </label>
